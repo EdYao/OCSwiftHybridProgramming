5475aeeee4316706a9211055c5a502cb1dc8ac58
Charles Yao <910818633@qq.com>
2017-05-28 23:50:01 +0800

    修改函数变量类型

--- OCSwiftComplex/NewViewController.swift
+++ OCSwiftComplex/NewViewController.swift
@@ -16,15 +16,15 @@ class NewViewController: UIViewController {
     
     @IBOutlet weak var textField: UITextField!
     @IBAction func changeTitleBtnClicked(_ sender: Any) {
-        if self.checkTitleLegal(title: textField.text! as NSString) {//check the content that user inputted legal
+        if self.checkTitleLegal(title: textField.text!) {//check the content that user inputted legal
             self.parentVC.changeTitle(self.textField.text);
             self.navigationController?.popViewController(animated: true)
         }
     }
     
-    func checkTitleLegal(title : NSString) -> Bool {
-        if title.length == 0 {
-            let alertView : UIAlertController = UIAlertController.init(title: "Error", message: "Please enter the title", preferredStyle: UIAlertControllerStyle(rawValue: 1)!);
+    func checkTitleLegal(title : String) -> Bool {
+        if title.isEmpty {
+            let alertView = UIAlertController.init(title: "Error", message: "Please enter the title", preferredStyle: UIAlertControllerStyle(rawValue: 1)!);
             alertView.addAction(UIAlertAction.init(title: "确定", style: UIAlertActionStyle(rawValue: 0)!, handler: { (UIAlertAction) in
                 alertView.dismiss(animated: true, completion: nil);
             }));
